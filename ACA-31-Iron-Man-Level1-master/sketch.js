6b9e66474d08223533aaeb01e327c20e1e243a30
vishwas2005 <84093562+vishwas2005@users.noreply.github.com>
2021-09-07 19:07:11 +0530

    Update sketch.js

--- ACA-31-Iron-Man-Level1-master/sketch.js
+++ ACA-31-Iron-Man-Level1-master/sketch.js
@@ -1,6 +1,8 @@
+//Making Variables
 var bg, backgroundImg;
 var score = 0
 
+//Preloading Images
 function preload() {
   backgroundImg = loadImage("images/bg.jpg");
   Iron = loadImage("images/iron.png")
@@ -9,6 +11,7 @@ function preload() {
   Sp = loadImage("images/spikes.png")
 }
 
+//For generating Spikes
 function genP(){
   Spi = createSprite(500,10,10,10)
   Spi.x = random(100,900)
@@ -19,6 +22,7 @@ function genP(){
   SpG.add(Spi)
 }
 
+//For generating diamonds
 function genD(){
   di = createSprite(500,10,10,10)
   di.x = random(100,900)
@@ -29,6 +33,7 @@ function genD(){
   diG.add(di)
 }
 
+//For generating stones
 function genS(){
   St = createSprite(500,10,10,60)
   St.x = random(100,900)
@@ -39,69 +44,90 @@ function genS(){
   StG.add(St)
 }
 
+//Setting all the characters on the canvas
 function setup() {
+  //Adding the background
   createCanvas(1000,550);
   bg = createSprite(580,300,1000,600);
   bg.addImage(backgroundImg)
   bg.velocityY = 4
-  bg.scale = 2 
+  bg.scale = 2
+
+  //Adding th player - Iron Man
   Man = createSprite(500,200,40,40)
   Man.addImage(Iron)
   Man.scale = 0.25
   ground=createSprite(200,550,1900,10)
+
+  //All the groups 
   StG = new Group()
   diG = new Group()
   SpG = new Group()
 }
 
+//Setting up how the game will function
 function draw() {
+  //Repeating the background
   if (bg.y > 600){
      bg.y = 300
-  }
+   }
  
+  //Controlling the player - Iron Man
   if(keyDown("up")){
-    Man.velocityY = -6
-  } 
+     Man.velocityY = -6
+   }
+
   if(keyDown("left")){
-    Man.x -= 6
-  }
+     Man.x -= 6
+   }
   if(keyDown("right")){
-    Man.x += 6
-  }
+     Man.x += 6
+   }
+  //Slowly increasing gravity
   Man.velocityY += 0.2 
 
+  //Generating the obstacles
   if(frameCount%40 == 0){
-    genS()
-  }
+     genS()
+   }
   if(frameCount%100 == 0){
-    genD()
-    genP()
-  }  
-  Man.collide(ground) 
+     genD()
+     genP()
+   }
+  Man.collide(ground)
+  //For Iron man to collide and stand on stones 
  for(g = 0; g < StG.length; g+=1){
-   h = StG.get(g)
-   if(Man.isTouching(h)){
-     Man.collide(h)
+     h = StG.get(g)
+     if(Man.isTouching(h)){
+       Man.collide(h)
+     }
    }
- }
- for(f = 0;f < diG.length;f += 1){
-  o = diG.get(f)
-   if(Man.isTouching(o)){
-     o.destroy()
-     score += 1
-     o = null
-   }
- }
- for(j = 0;j < SpG.length;j += 1){
-   k = SpG.get(j)
-   if(Man.isTouching(k)){
-      k.destroy()
-      k = null
-      score -= 5
-   }
- }
 
+  //For Iron man to collect coins  
+ for(f = 0;f < diG.length;f += 1){
+     o = diG.get(f)
+     if(Man.isTouching(o)){
+       o.destroy()
+       score += 1
+       o = null
+     }
+   }
+  
+
+  //For reducing points if hit by Spikes 
+ for(j = 0;j < SpG.length;j += 1){
+     k = SpG.get(j)
+     if(Man.isTouching(k)){
+       k.destroy()
+       k = null
+       score -= 5
+     }
+   }
+
+  //Drawing all the characters
   drawSprites();
+
+  //To show the score on the screen
    fill("white")
    textSize(20)
    stroke("blue")
